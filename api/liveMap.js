e5f39ea57622f8d70c34eb92d1d49f6aa3b36855
thehaigo <sirannha@gmail.com>
2021-08-29 16:23:07 +0900

    attach auth token

--- api/liveMap.js
+++ api/liveMap.js
@@ -1,9 +1,21 @@
 import axios from "axios";
-
+import AsyncStorage from "@react-native-async-storage/async-storage";
 let url = "http://localhost:4000/api/";
 
 const liveMapApi = axios.create({
   baseURL: url,
 });
 
+liveMapApi.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer: ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 export { liveMapApi };
