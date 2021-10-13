20c9d209f53ee3e42e522e205536b8e9ec9a6e5b
thehaigo <sirannha@gmail.com>
2021-10-13 14:48:55 +0900

    uuid add

--- api/liveMap.js
+++ api/liveMap.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { v4 as uuidv4 } from "uuid";
 let url = "http://localhost:4000/api/";
 
 const liveMapApi = axios.create({
@@ -9,8 +10,14 @@ const liveMapApi = axios.create({
 liveMapApi.interceptors.request.use(
   async (config) => {
     const token = await AsyncStorage.getItem("token");
+    let uuid = await AsyncStorage.getItem("uuid");
+    if (!uuid) {
+      uuid = uuidv4();
+      await AsyncStorage.setItem("uuid", uuid);
+    }
     if (token) {
       config.headers.Authorization = `Bearer: ${token}`;
+      config.headers.UUID = uuid;
     }
     return config;
   },
